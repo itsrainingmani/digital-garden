d487b0b85f33e9362ce1a46a2d1a962583c0e2fa
Manikandan Sundararajan <mani@hey.com>
2022-08-13 01:30:11 -0400

    Construct a title from either the provided title or from the md heading

--- src/write.rs
+++ src/write.rs
@@ -1,6 +1,6 @@
 use color_eyre::{eyre::Context, Result};
 use edit::{edit_file, Builder};
-use std::io::{Read, Write};
+use std::io::{Read, Seek, SeekFrom, Write};
 use std::path::PathBuf;
 
 const TEMPLATE: &[u8; 2] = b"# ";
@@ -23,9 +23,24 @@ pub fn write(garden_path: PathBuf, title: Option<String>) -> Result<()> {
 
     // read the user's changes back from the file into a string
     let mut contents = String::new();
+
+    // When we write the TEMPLATE string, the file automatically seeks forward by 2 bytes
+    // So when we read it to string, it skips over the `# `
+    file.seek(SeekFrom::Start(0))?;
     file.read_to_string(&mut contents)?;
 
-    dbg!(contents);
+    // use title if passed in
+    // otherwise try to find a heading in the markdown
 
+    let document_title = title.or_else(|| {
+        contents
+            .lines()
+            .find(|v| v.starts_with("# "))
+            // markdown headings are required to start with a `# `
+            // with atleast one space
+            .map(|maybe_line| maybe_line.trim_start_matches("# ").to_string())
+    });
+
+    dbg!(contents, document_title);
     todo!()
 }
